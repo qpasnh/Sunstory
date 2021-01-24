23e17a06c33025ac843ad427a666f2026d31f6bd
Subilan <suyuanliang1995@gmail.com>
2021-01-24 17:15:29 +0800

    feat: animejs install, global less

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import anime from "@/animejs.install"
+import "@/css/global.less";
 
 Vue.config.productionTip = false
+
+Vue.prototype.$anime = anime;
 
 new Vue({
   router,
