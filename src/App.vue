777722c5b5292ea45971e5f407fdf02d67597d69
Subilan <suyuanliang1995@gmail.com>
2021-01-24 19:58:22 +0800

    view: main app

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app">
+        <navbar/>
+        <div class="main-content">
+            <router-view/>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Navbar from "@/components/Navbar.vue"
+
+export default Vue.extend({
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        Navbar
+    }
+})
+</script>
