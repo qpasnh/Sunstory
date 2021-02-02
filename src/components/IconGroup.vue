68480a652667f9b5d7484f6e45b1666d62aae1e8
Subilan <suyuanliang1995@gmail.com>
2021-02-02 14:54:16 +0800

    comp: icon group for multiple icon els

---
+++ src/components/IconGroup.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="icon-group">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.icon-group {
+    display: flex;
+    align-items: center;
+
+    > * {
+        margin: 0 8px 0 8px;
+    }
+}
+</style>
