5fb43c5ed641f5530e3055135ab7ddaff205fff6
Subilan <suyuanliang1995@gmail.com>
2021-02-06 16:55:26 +0800

    fix: avoid unnecessary warning of dupjump

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,16 +3,24 @@ import VueRouter, { RouteConfig } from "vue-router";
 
 Vue.use(VueRouter);
 
+const originalPush = VueRouter.prototype.push;
+
+// @ts-ignore
+VueRouter.prototype.push = location => {
+  // @ts-ignore
+  return originalPush.call(this, location).catch((err) => err);
+};
+
 const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "home",
-    component: () => import("@/views/Home.vue")
+    component: () => import("@/views/Home.vue"),
   },
   {
     path: "/gallery",
     name: "gallery",
-    component: () => import("@/views/Gallery.vue")
+    component: () => import("@/views/Gallery.vue"),
   },
 ];
 
