0bc912a99009208dafb2866db9e04e6014d3d601
Subilan <suyuanliang1995@gmail.com>
2021-02-13 19:34:37 +0800

    upd: pwa icon

--- vue.config.js
+++ vue.config.js
@@ -3,18 +3,86 @@ const path = require("path");
 module.exports = {
   pwa: {
     name: "SoTap",
-    themeColor: "#fcd307",
-    msTileColor: "#fcd307",
+    themeColor: "#fff",
+    msTileColor: "#fff",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "default",
     workboxPluginMode: "GenerateSW",
-    iconPaths: {
-      faviconSVG: null,
-      favicon32: "@/assets/icons/logo-32x32.png",
-      favicon16: "@/assets/icons/logo-16x16.png",
-      appleTouchIcon: null,
-      maskIcon: null,
-      msTileImage: "@/assets/icons/logo-144x144.png",
+    manifestOptions: {
+      icons: [
+        {
+          src: "./img/icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-60x60.png",
+          sizes: "60x60",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-76x76.png",
+          sizes: "76x76",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-120x120.png",
+          sizes: "120x120",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-152x152.png",
+          sizes: "152x152",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-180x180.png",
+          sizes: "180x180",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon.png",
+          sizes: "180x180",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/favicon-16x16.png",
+          sizes: "16x16",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/favicon-32x32.png",
+          sizes: "32x32",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/msapplication-icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/mstile-150x150.png",
+          sizes: "150x150",
+          type: "image/png",
+        },
+      ],
     },
   },
   pluginOptions: {
